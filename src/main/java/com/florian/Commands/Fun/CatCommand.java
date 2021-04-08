0a8da5a6a5e63babf87a1434d12108572b0126a0
Florian <66821749+floriankorstanje@users.noreply.github.com>
2021-04-08 18:58:26 +0200

    Ok i will test it now

--- src/main/java/com/florian/Commands/Fun/CatCommand.java
+++ src/main/java/com/florian/Commands/Fun/CatCommand.java
@@ -3,13 +3,12 @@ package com.florian.Commands.Fun;
 import com.florian.Commands.BaseCommand;
 import com.florian.Commands.CommandType;
 import com.florian.ErrorCode;
-import com.florian.Vars;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class CatCommand extends BaseCommand {
     public CatCommand() {
         super.command = "cat";
-        super.description = "Sends " + Vars.botOwner.getName() + "'s favorite cat image!";
+        super.description = "Sends my favorite cat image!";
         super.advancedDescription = "I want to change this to a random cat image in the future, but I haven't found a nice API yet :(";
         super.commandType = CommandType.FUN;
     }
